59edd8e4d13026e0ddddf637621a98f4c782f486
Robinson <robinquiroz57@gmail.com>
2025-04-03 20:21:39 -0500

    feat el banner en el componente banner queda del ancho de la pantalla

--- src/components/banner.jsx
+++ src/components/banner.jsx
@@ -4,7 +4,7 @@ export default function Banner() {
   return (
     <>
         {/* <!-- Header --> */}
-        <header class="w3-display-container w3-content" style={{"maxWidth":"1500px;"}}>
+        <header class="w3-display-container w3-content" style={{"maxWidth":"1500px"}}>
         <img class="w3-image" src="https://www.w3schools.com/w3images/hotel.jpg" alt="The Hotel" style={{"minWidth":"1000px"}} width="1500" height="800"/>
         <div class="w3-display-left w3-padding w3-col l6 m8">
             <div class="w3-container w3-red">
@@ -12,7 +12,7 @@ export default function Banner() {
             </div>
             <div class="w3-container w3-white w3-padding-16">
             <form>
-                <div class="w3-row-padding" style={{"margin":"0 -16px;"}}>
+                <div class="w3-row-padding" style={{"margin":"0 -16px"}}>
                 <div class="w3-half w3-margin-bottom">
                     <label><i class="fa fa-calendar-o"></i> Check In</label>
                     <input class="w3-input w3-border" type="text" placeholder="DD MM YYYY" name="CheckIn" required/>
@@ -22,7 +22,7 @@ export default function Banner() {
                     <input class="w3-input w3-border" type="text" placeholder="DD MM YYYY" name="CheckOut" required/>
                 </div>
                 </div>
-                <div class="w3-row-padding" style={{"margin":"8px -16px;"}}>
+                <div class="w3-row-padding" style={{"margin":"8px -16px"}}>
                 <div class="w3-half w3-margin-bottom">
                     <label><i class="fa fa-male"></i> Adults</label>
                     <input class="w3-input w3-border" type="number" value="1" name="Adults" min="1" max="6"/>
